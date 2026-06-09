4061372ffcd6e1b09d540c75af5c4c2d53466a14
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-09 12:01:06 +0530

    variables&comments

---
+++ 01-cpp-basics/1-input-output/variables&comments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//this program was created by me
+/* This is a multi-line comment
+   It can span multiple lines
+   and is useful for longer explanations */
+
+   int main() {
+    int sum=6;
+   cout << "Hello World!" << sum; // This is a single-line comment
+    return 0;
+}
