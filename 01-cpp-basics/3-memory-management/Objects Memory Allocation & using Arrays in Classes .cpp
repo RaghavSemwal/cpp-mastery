00391b142666d7fbcbc0f8327eec8427bae2fc91
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-11 23:37:22 +0530

    Static Data Members Methods

--- 01-cpp-basics/3-memory-management/Objects Memory Allocation & using Arrays in Classes .cpp
+++ 01-cpp-basics/3-memory-management/Objects Memory Allocation & using Arrays in Classes .cpp
@@ -39,3 +39,98 @@ int main() {
         s[i].display();
     }
 }
+
+
+/*📌 Key Takeaways
+Each object gets its own memory for data members.
+
+Arrays can be:
+
+Inside a class (marks[5])
+
+Array of objects (Student s[10])
+*/
+
+/* 
+                    Memory Diagram:
+Object s1:                     Object s2:
++-----------------------+      +-----------------------+
+| roll   (4B)           |      | roll   (4B)           |
++-----------------------+      +-----------------------+
+| marks  (4B)           |      | marks  (4B)           |
++-----------------------+      +-----------------------+
+
+
+Student s[3];
+
+Index 0 (s[0])              Index 1 (s[1])              Index 2 (s[2])
++-------------+             +-------------+             +-------------+
+| roll (4B)   |             | roll (4B)   |             | roll (4B)   |
++-------------+             +-------------+             +-------------+
+| marks (4B)  |             | marks (4B)  |             | marks (4B)  |
++-------------+             +-------------+             +-------------+
+
+<--------- contiguous memory block --------->
+
+✔ All objects stored back‑to‑back in memory
+
+✔ Total memory = 3 × sizeof(Student) 
+*/
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+class Shop {
+private:
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) {
+        counter = 0;
+    }
+
+    void setPrice(void) {
+        cout << "Enter ID of item no. " << counter + 1 << ": ";
+        cin >> itemId[counter];
+
+        cout << "Enter Price of item: ";
+        cin >> itemPrice[counter];
+
+        counter++;
+    }
+
+    void displayPrice(void) {
+        cout << "\n--- Price List ---\n";
+        for (int i = 0; i < counter; i++) {
+            cout << "Item ID: " << itemId[i]
+                 << "  Price: " << itemPrice[i] << endl;
+        }
+    }
+};
+
+int main() {
+    Shop s;
+    s.initCounter();
+
+    int n;
+    cout << "How many items do you want to enter? ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        s.setPrice();
+    }
+
+    s.displayPrice();
+
+    return 0;
+}
