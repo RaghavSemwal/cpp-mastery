ffb35644760d08b003f26e1afaa75e57b27b54af
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-15 19:58:53 +0530

    copy constructor

--- 02-oop-concepts/1-classes-objects/copy constructor.cpp
+++ 02-oop-concepts/1-classes-objects/copy constructor.cpp
@@ -1,3 +1,18 @@
+/*⭐ Copy Constructor
+🎯 Definition
+A copy constructor is a special constructor that creates a new object as a copy of an existing object.
+
+⭐ Why Copy Constructor Exists
+To initialize one object from another
+
+Ensures proper copying of data members
+
+Useful when objects contain dynamic memory (to avoid shallow copy probl
+*/
+
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -35,3 +50,17 @@ int main() {
 
     return 0;
 }
+
+/*Copy constructor takes a reference to another object of the same class.
+
+Default copy constructor is provided by compiler (does shallow copy).
+
+You can define your own for deep copy (important when using pointers).
+
+Called when:
+
+A new object is created from an existing one (Student s2(s1);)
+
+Passed by value to a function
+
+Returned from a function */
