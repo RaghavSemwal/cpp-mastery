6951a5bb5a2845d53331c9dda86064f78a9db4eb
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-16 13:07:46 +0530

    destructor

---
+++ 02-oop-concepts/1-classes-objects/destructor.cpp
@@ -0,0 +1,55 @@
+/*⭐ Destructor in C++
+🎯 Definition
+A destructor is a special member function of a class that:
+
+Has the same name as the class, preceded by a tilde (~)
+
+Has no return type and no parameters
+
+Is automatically called when an object goes out of scope or is deleted
+
+Used to release resources (like memory, files, connections)*/
+
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int roll;
+    float marks;
+
+public:
+    // Parameterized Constructor
+    Student(int r, float m) {
+        roll = r;
+        marks = m;
+        cout << "Constructor Called!" << endl;
+    }
+
+    // Copy Constructor
+    Student(const Student &s) {
+        roll = s.roll;
+        marks = s.marks;
+        cout << "Copy Constructor Called!" << endl;
+    }
+
+    // Destructor
+    ~Student() {
+        cout << "Destructor Called for Roll: " << roll << endl;
+    }
+
+    void display() {
+        cout << "Roll: " << roll << ", Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student s1(101, 95.5);   // constructor runs
+    Student s2(s1);          // copy constructor runs
+
+    s1.display();
+    s2.display();
+
+    // destructors will be called automatically at end of scope
+    return 0;
+}
