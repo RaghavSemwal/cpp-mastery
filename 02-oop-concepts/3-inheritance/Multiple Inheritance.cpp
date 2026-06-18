d267ff73f5c311a4198b91aed8d726a53e00858e
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-18 23:13:12 +0530

    Multiple Inheritance Deep Dive

--- 02-oop-concepts/3-inheritance/Multiple Inheritance.cpp
+++ 02-oop-concepts/3-inheritance/Multiple Inheritance.cpp
@@ -3,46 +3,46 @@ Definition: A derived class inherits from two or more base classes.
 
 Purpose: Allows combining features from multiple classes into one.*/
 
-#include <iostream>     
-using namespace std;
+// #include <iostream>     
+// using namespace std;
 
-class base1 {
-    protected :
-    int num1;
-    public:
-    void setNum1(int n1) {
-        num1 = n1;
-    }
-};
+// class base1 {
+//     protected :
+//     int num1;
+//     public:
+//     void setNum1(int n1) {
+//         num1 = n1;
+//     }
+// };
 
-class base2 {
-    protected :
-    int num2;
-    public:
-    void setNum2(int n2) {
-        num2 = n2;
-    }
-};
+// class base2 {
+//     protected :
+//     int num2;
+//     public:
+//     void setNum2(int n2) {
+//         num2 = n2;
+//     }
+// };
 
-class derive :public base1,public base2 {
-    public:
-    void display() {
-        cout << "Num1: " << num1 << endl;
-        cout << "Num2: " << num2 << endl;
-    }
-    void sum() {
-        cout << "Sum: " << num1 + num2 << endl;
-    }
-};
+// class derive :public base1,public base2 {
+//     public:
+//     void display() {
+//         cout << "Num1: " << num1 << endl;
+//         cout << "Num2: " << num2 << endl;
+//     }
+//     void sum() {
+//         cout << "Sum: " << num1 + num2 << endl;
+//     }
+// };
 
-int main() {
-    derive d;
-    d.setNum1(10);  // from base1
-    d.setNum2(20);  // from base2
-    d.display();    // shows both numbers
-    d.sum();        // shows sum of both numbers
-    return 0;
-}
+// int main() {
+//     derive d;
+//     d.setNum1(10);  // from base1
+//     d.setNum2(20);  // from base2
+//     d.display();    // shows both numbers
+//     d.sum();        // shows sum of both numbers
+//     return 0;
+// }
 
 
 
