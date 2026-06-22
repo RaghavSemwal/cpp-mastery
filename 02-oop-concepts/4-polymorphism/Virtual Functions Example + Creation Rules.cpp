fd97cbb702204352ee026df9d251237708d86807
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-22 21:05:47 +0530

    Virtual Functions Example + Creation Rules

--- 02-oop-concepts/4-polymorphism/Virtual Functions Example + Creation Rules.cpp
+++ 02-oop-concepts/4-polymorphism/Virtual Functions Example + Creation Rules.cpp
@@ -17,3 +17,64 @@ They can be friends of another class.
 A virtual function in base class might not be used.
 
 If defined in base, no need to redefine in derived (optional).*/
+
+#include <iostream>
+using namespace std;
+
+class CWH {
+protected:
+    string title;
+    float rating;
+public:
+    CWH(string s, float r) {
+        title = s;
+        rating = r;
+    }
+    virtual void display() {   // virtual function
+        cout << "Base class display (generic content)\n";
+    }
+};
+
+class CWHVideo : public CWH {
+    float videoLength;
+public:
+    CWHVideo(string s, float r, float vl) : CWH(s, r) {
+        videoLength = vl;
+    }
+    void display() override {
+        cout << "This is an amazing video with title " << title << endl;
+        cout << "Ratings: " << rating << " out of 5 stars" << endl;
+        cout << "Length of this video is: " << videoLength << " minutes" << endl;
+    }
+};
+
+class CWHText : public CWH {
+    int words;
+public:
+    CWHText(string s, float r, int w) : CWH(s, r) {
+        words = w;
+    }
+    void display() override {
+        cout << "This is an amazing text tutorial with title " << title << endl;
+        cout << "Ratings: " << rating << " out of 5 stars" << endl;
+        cout << "Number of words in this tutorial: " << words << endl;
+    }
+};
+
+int main() {
+    CWHVideo video("C++ OOP Tutorial", 4.8, 15.5);
+    CWHText text("Polymorphism Notes", 4.6, 1200);
+
+    // Base class pointer
+    CWH *ptr;
+
+    // Pointing to video object
+    ptr = &video;
+    ptr->display();   // calls CWHVideo::display()
+
+    // Pointing to text object
+    ptr = &text;
+    ptr->display();   // calls CWHText::display()
+
+    return 0;
+}
