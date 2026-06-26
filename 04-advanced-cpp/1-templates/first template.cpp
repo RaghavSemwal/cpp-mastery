5eac8c7a60092ce05b97dfdba0a7d9faf9837158
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-26 23:49:25 +0530

    Writing our First C++ Template in VS Code

--- 04-advanced-cpp/1-templates/first template.cpp
+++ 04-advanced-cpp/1-templates/first template.cpp
@@ -10,16 +10,53 @@ Two types:
 Function Templates
 
 Class Templates*/
+
+
+// #include <iostream>
+// using namespace std;
+
+// template <class T>
+// T add(T a, T b) {
+//     return a + b;
+// }
+
+// int main() {
+//     cout << "Int sum: " << add<int>(3, 4) << endl;
+//     cout << "Float sum: " << add<float>(3.5, 4.2) << endl;
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
 template <class T>
-T add(T a, T b) {
-    return a + b;
-}
+class Vector {
+public:
+    T *arr;
+    int size;
+
+    Vector(int m) {
+        size = m;
+        arr = new T[size];
+    }
+
+    T dotproduct(Vector &v) {
+        T d = 0;
+        for (int i = 0; i < size; i++) {
+            d += this->arr[i] * v.arr[i];
+        }
+        return d;
+    }
+};
 
 int main() {
-    cout << "Int sum: " << add<int>(3, 4) << endl;
-    cout << "Float sum: " << add<float>(3.5, 4.2) << endl;
+    Vector<float> v1(3);
+    v1.arr[0] = 1.1; v1.arr[1] = 2.2; v1.arr[2] = 3.3;
+
+    Vector<float> v2(3);
+    v2.arr[0] = 1.0; v2.arr[1] = 2.0; v2.arr[2] = 3.0;
+
+    cout << "Dot product = " << v1.dotproduct(v2) << endl;
     return 0;
 }
