8ab24667b78bbe3ec4eac9e352d43901b68433db
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-26 23:42:45 +0530

    Writing our First C++ Template in VS Code

--- 04-advanced-cpp/1-templates/first template.cpp
+++ 04-advanced-cpp/1-templates/first template.cpp
@@ -1,42 +1,25 @@
+/*
+⭐ Writing Our First C++ Template in VS Code
+1. What is a Template?
+A template allows writing generic code that works with any data type (int, float, double, etc.).
+
+Supports code reusability and type safety.
+
+Two types:
+
+Function Templates
+
+Class Templates*/
 #include <iostream>
 using namespace std;
 
-
-template<class T>
-class Vector {
-public:
-    T *arr;
-    int size;
-
-    Vector(int m) {
-        size = m;
-        arr = new T[size];
-    }
-
-    T dotproduct(Vector &v) {
-        T d = 0;
-        for (int i = 0; i < size; i++) {
-            d += this->arr[i] * v.arr[i];   // ✅ multiply with v’s array
-        }
-        return d;
-    }
-};
-
-int main() {
-    Vector <float> v1(3);
-    v1.arr[0] = 1.09;
-    v1.arr[1] = 2.9;
-    v1.arr[2] = 3.4;
-
-    Vector <float> v2(3);
-    v2.arr[0] = 1.44;
-    v2.arr[1] = 2.999;
-    v2.arr[2] = 3.09;
-
-    float a = v1.dotproduct(v2);
-    cout << "Dot product = " << a << endl;
-
-    return 0;
+template <class T>
+T add(T a, T b) {
+    return a + b;
 }
 
-
+int main() {
+    cout << "Int sum: " << add<int>(3, 4) << endl;
+    cout << "Float sum: " << add<float>(3.5, 4.2) << endl;
+    return 0;
+}
