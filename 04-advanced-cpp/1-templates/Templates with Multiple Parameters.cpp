adef3102b563fa579b8c4e5257d835ce367e9436
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-27 18:58:53 +0530

    C++ Templates: Templates with Multiple Parameters

--- 04-advanced-cpp/1-templates/Templates with Multiple Parameters.cpp
+++ 04-advanced-cpp/1-templates/Templates with Multiple Parameters.cpp
@@ -1,3 +1,32 @@
+/*
+⭐ C++ Templates: Templates with Multiple Parameters
+🎯 What It Means
+A template can take more than one type parameter.
+This allows you to create classes or functions that work with two or more different data types at the same time.
+
+Think of it as a generic blueprint that accepts multiple types*/
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// template <class T1, class T2>
+// void display(T1 a, T2 b) {
+//     cout << "A = " << a << ", B = " << b << endl;
+// }
+
+// int main() {
+//     display<int, float>(5, 3.14f);
+//     display<string, int>("Raghav", 21);
+//     return 0;
+// }
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -23,3 +52,16 @@ int main(){
         obj.display();
         return 0;
 }
+
+
+/*
+TODO:⭐ Why Use Multiple Template Parameters?
+
+When your class/function needs two different types  
+(e.g., key–value pairs, coordinates, maps, complex numbers)
+
+More flexibility and reusability
+
+Avoids writing multiple overloaded versions
+*/
+
