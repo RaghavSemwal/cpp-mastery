c9626b9cb9b3185c5d7a46ebc0f6c8dd4fe91688
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-27 18:53:57 +0530

    C++ Templates: Templates with Multiple Parameters

---
+++ 04-advanced-cpp/1-templates/Templates with Multiple Parameters.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+template <class T1, class T2>
+class myClass{
+    public:
+    T1 data1;
+    T2 data2;
+
+     myClass(T1 a, T2 b){
+        data1=a;
+        data2=b;
+     }
+
+     void display(){
+        cout<<data1<<endl;
+        cout<<data2<<endl;
+       }
+};
+
+int main(){
+        myClass<char,float>obj('d',3.3);
+        obj.display();
+        return 0;
+}
