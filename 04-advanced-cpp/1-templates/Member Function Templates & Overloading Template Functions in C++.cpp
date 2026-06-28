ae4ce970dfc6c6c8f5ffb1e4fced8133ecd01d30
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-28 20:02:44 +0530

    Member Function Templates & Overloading Template Functions

--- 04-advanced-cpp/1-templates/Member Function Templates & Overloading Template Functions in C++.cpp
+++ 04-advanced-cpp/1-templates/Member Function Templates & Overloading Template Functions in C++.cpp
@@ -11,31 +11,76 @@ The class stores one type
 But a function inside the class must work with many types
 */
 
+// #include <iostream>
+// using namespace std;
+
+// class MyClass {
+// public:
+//     int data;
+
+//     MyClass(int x) {
+//         data = x;
+//     }
+
+//     // Member function template
+//     template <class T>
+//     void show(T value) {
+//         cout << "Data = " << data << endl;
+//         cout << "Value = " << value << endl;
+//     }
+// };
+
+// int main() {
+//     MyClass obj(10);
+
+//     obj.show(3.14);     // T = float
+//     obj.show('A');      // T = char
+//     obj.show("Raghav"); // T = const char*
+
+//     return 0;
+// }
+
+
+/*
+
+⭐ Key Points
+->Class is not a template.
+
+->Only the function show() is a template.
+
+->Compiler generates different versions of show() depending on the argument type. 
+  */
+
+
+  /*
+  ⭐ 2. Overloading Template Functions
+🎯 What It Means
+You can overload a template function by:
+
+Writing a normal function with the same name
+
+Writing another template with different parameters
+
+The compiler chooses the best match.*/
+
 #include <iostream>
 using namespace std;
 
-class MyClass {
-public:
-    int data;
+// Template function
+template <class T>
+void display(T x) {
+    cout << "Template display: " << x << endl;
+}
 
-    MyClass(int x) {
-        data = x;
-    }
-
-    // Member function template
-    template <class T>
-    void show(T value) {
-        cout << "Data = " << data << endl;
-        cout << "Value = " << value << endl;
-    }
-};
+// Normal overloaded function
+void display(int x) {
+    cout << "Normal display (int): " << x << endl;
+}
 
 int main() {
-    MyClass obj(10);
-
-    obj.show(3.14);     // T = float
-    obj.show('A');      // T = char
-    obj.show("Raghav"); // T = const char*
+    display(10);       // calls normal function (best match)
+    display(3.14);     // calls template
+    display('A');      // calls template
 
     return 0;
 }
