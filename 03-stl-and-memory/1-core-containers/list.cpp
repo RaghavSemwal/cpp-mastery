94b7887c91b2df1bb8f1e6db0d0e927f9606f13d
RaghavSemwal <raghav9tha35@gmail.com>
2026-06-30 13:46:57 +0530

    list

--- 03-stl-and-memory/1-core-containers/list.cpp
+++ 03-stl-and-memory/1-core-containers/list.cpp
@@ -93,9 +93,7 @@ int main() {
     // -------------------------------
     // 5. Reverse list1
     // -------------------------------
-    list1.reverse();
-    cout << "List1 after reversing: ";
-    display(list1);
+   
 
     return 0;
 }
